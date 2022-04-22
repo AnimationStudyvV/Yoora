a74ba82ca8d04484c44be80aed83f17e99f36f2b
yunanah <bbyl6319@gmail.com>
2022-04-22 17:34:53 +0900

    💫 ✨ add clock flipping animation

--- src/components/FlipClock/index.tsx
+++ src/components/FlipClock/index.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Clock, ClockContainer, Digit } from "./styles";
+import {
+  Card,
+  CardBackFace,
+  CardFrontFace,
+  Clock,
+  ClockContainer,
+  Digit,
+} from "./styles";
 
 const FlipClock: React.FC = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
   return (
     <ClockContainer>
       <Clock>
-        <Digit prev="0" next="1"></Digit>
+        <Digit data-digit-before="0" data-digit-after="1">
+          <Card
+            className={isFlipped ? "flipped" : ""}
+            onClick={() => {
+              setIsFlipped(!isFlipped);
+            }}
+          >
+            <CardFrontFace>0</CardFrontFace>
+            <CardBackFace>1</CardBackFace>
+          </Card>
+        </Digit>
+        <Digit data-digit-before="0" data-digit-after="1">
+          <Card
+            className={isFlipped ? "flipped" : ""}
+            onClick={() => {
+              setIsFlipped(!isFlipped);
+            }}
+          >
+            <CardFrontFace>0</CardFrontFace>
+            <CardBackFace>1</CardBackFace>
+          </Card>
+        </Digit>
       </Clock>
     </ClockContainer>
   );
--- src/components/FlipClock/styles.tsx
+++ src/components/FlipClock/styles.tsx
@@ -5,18 +5,29 @@ export const ClockContainer = styled.div`
   justify-content: center;
   align-items: center;
 
-  font-size: 44px;
+  font-size: 50px;
+  line-height: 0;
+
+  & * {
+    user-select: none;
+    cursor: default;
+  }
 `;
 
-export const Clock = styled.div``;
+export const Clock = styled.div`
+  display: flex;
 
-export const Digit = styled.div.attrs(
-  (props: { prev: string; next: string }) => props
-)`
+  perspective: 1000px;
+`;
+
+export const Digit = styled.div`
   position: relative;
-  width: 40px;
-  height: 75px;
+
+  width: 45px;
+  height: 80px;
   box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  margin-left: 2px;
 
   &::before,
   &::after {
@@ -24,17 +35,99 @@ export const Digit = styled.div.attrs(
     z-index: 0;
     display: flex;
     justify-content: center;
-
+    border-radius: 4px;
     width: 100%;
     height: 50%;
     overflow: hidden;
+
+    background: white;
+    color: black;
+    background: linear-gradient(0, #aaa, #ccc, 30%, #fafafa);
   }
 
   &::before {
-    content: attr(${(props) => `${props.prev}`});
+    content: attr(data-digit-before);
+
+    bottom: 0;
+    align-items: flex-start;
   }
 
   &::after {
-    content: attr(${(props) => `${props.next}`});
+    content: attr(data-digit-after);
+
+    top: 0;
+    align-items: flex-end;
+  }
+`;
+
+export const Card = styled.div`
+  position: relative;
+  z-index: 1;
+
+  width: 100%;
+  height: 50%;
+
+  transform-style: preserve-3d;
+  transform-origin: bottom;
+
+  transform: rotateX(0);
+  transition: transform 0.7s ease-in-out;
+
+  border-radius: 4px;
+
+  &.flipped {
+    transform: rotateX(-180deg);
+  }
+`;
+export const CardFrontFace = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  border-radius: 4px;
+  background: white;
+  color: black;
+
+  align-items: flex-end;
+  backface-visibility: hidden;
+
+  &,
+  ${Digit}::after {
+    background: linear-gradient(0, #aaa, #ccc, 30%, #fafafa);
+    border-bottom: 1.5px solid black;
+    border-radius: 4px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+export const CardBackFace = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  border-radius: 4px;
+
+  background: white;
+  color: black;
+
+  align-items: flex-start;
+  transform: rotateX(-180deg);
+  backface-visibility: hidden;
+
+  &,
+  ${Digit}::before {
+    background: linear-gradient(0, #aaa, #ccc, 30%, #fafafa);
+    border-top: 1.5px solid black;
+    border-radius: 4px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
   }
 `;
